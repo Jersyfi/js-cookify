98308de175fe9c91c563e129eacdc4699a2a10db
Jérôme Bastian Winkel <64214261+Jersyfi@users.noreply.github.com>
2022-12-28 14:17:31 +0100

    Preview Bug Fixes

--- test/preview/index.js
+++ test/preview/index.js
@@ -1,22 +1,18 @@
 import Cookify from '../../src/index.js'
 
 var cookify = new Cookify('cookie_consent', () => {
-        document.getElementById('manage').classList.add('d-none')
+        document.getElementById('manage').style.display = 'none'
     }, (data) => {
         console.log(data)
     }, false, false, 'necessary', () => {
         //show the cookie popup as long as the user has not seen it
         if (cookify.getDataState(cookify.viewedName)) {
-            document.getElementById('manage').style.display = 'none';
+            document.getElementById('manage').style.display = 'none'
         } else {
-            document.getElementById('manage').style.display = 'block';
+            document.getElementById('manage').style.display = 'block'
         }
     })
 
 document.getElementById('open').addEventListener('click', function () {
-    document.getElementById('manage').classList.remove('d-none')
+    document.getElementById('manage').style.display = 'block'
 })
-
-if (!cookify.getDataState(cookify.viewedName)) {
-    document.getElementById('manage').classList.remove('d-none')
-}
